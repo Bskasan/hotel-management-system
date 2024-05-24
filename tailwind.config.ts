4cada99276a256251c184684f5cd6f4fe40b096c
Bskasan <b.kasan@yandex.com>
2024-05-24 18:20:46 +0300

    done - completed header component for now

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         primary: "#038C7F",
         secondary: "#F2C641",
         tertiary: {
-          dark: "#F27405",
+          dark: "#939393",
           light: "#F2C641",
         },
       },
